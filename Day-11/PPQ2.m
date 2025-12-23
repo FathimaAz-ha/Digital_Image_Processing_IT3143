dd955d1966efa8e5020e91ae2d42845140e0d8fc
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-12-23 21:12:50 +0530

    Fix comparison in pixel replacement logic

--- Day-11/PPQ2.m
+++ Day-11/PPQ2.m
@@ -17,7 +17,7 @@ subtractTwoImages(I2grey, I1grey);
 for i = size(I1, 1)
     for j = 1 : size(I1, 2)
 
-        if I1(i,j) < I2
+        if I1(i,j) < I2(i,j)
             I2(i,j) = I1(i,j); 
         end
     end
